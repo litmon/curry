36c5b48fed06cd059000b51ccd3f6e07d1d9398f
Fukuo Kadota <app.fk91@gmail.com>
2015-04-22 16:50:56 +0900

    ちょっと綺麗にしました

--- curry.rb
+++ curry.rb
@@ -1,8 +1,8 @@
 
-curry = ["よそえました。", "よそえませんでした。", "そもそもかけませんでした。"]
+curry = ["よそえました。", "よそえませんでした。"]
 
 if ARGV.length <= 0
-  puts "誰にカレーをよそいますか？"
+  print "誰にカレーをよそいますか？"
   puts gets.chomp + "はカレーを" + curry.sample
 elsif ARGV[0]
   puts ARGV[0] + "はカレーを" + curry.sample
